db0c23e4cf7da22da985a591e64feec50d87ea87
rails <lakhveersr@shriffle.com>
2023-07-27 12:24:40 +0530

    Jira Verification

--- Admin.rb
+++ Admin.rb
@@ -131,278 +131,278 @@ def admin
         k=0
         h.each_key { |key| 
           h[key]=acc[k] unless k==1
-        k+=1 }
-        h["Full Name"]=h["Full Name"].to_s+" "+h["Last Name"].to_s
-        h.delete "Last Name"
-        puts h
-      end
-      f1.close
-    when 3
-      puts "Deleting Accountant"
-      puts "Enter the User ID"
-      usid=gets.chomp.to_s
-      Accountant.delete(usid)
-    when 4
-      puts "Adding a Cashier"
-    
-      loop do
-        puts "Enter First Name"
-        @fname=gets.chomp.to_s
-        unless @fname.empty?
-          break
-        else
-          puts "Could Not Be Empty"
+          k+=1 }
+          h["Full Name"]=h["Full Name"].to_s+" "+h["Last Name"].to_s
+          h.delete "Last Name"
+          puts h
         end
-      end
-
-      loop do
-        puts "Enter Last Name"
-        @lname=gets.chomp.to_s
-        unless @lname.empty?
-          break
-        else
-          puts "Could Not Be Empty"
-        end
-      end
-
-      loop do
-        puts "Enter Id Proof No."
-        @idp=gets.chomp.to_s
-        unless @idp.empty?
-          break
-        else
-          puts "Could Not Be Empty"
-        end
-      end
-
-      loop do
-        puts "Enter User Id"
-        @usid=gets.chomp.to_s
-        unless @usid.empty?
-          flag=true
-
-          while flag do
-            c=0
-            f=File.open("Cashier.txt","a+")
-            f.each do |line|
-              if line.match /^#{@usid}/
-                puts "User Id Already Taken"
-                c+=1
-              end
-            end
-            flag=false if c==0
-          end
-          f.close
-          break
-        else
-          puts "Could Not be Empty"
-        end
-      end
-
-      while true do
-        puts "Enter Your New Password"
-        @pass=STDIN.noecho(&:gets).chomp.to_s
-        puts "Confirm Password"
-        @npass=STDIN.noecho(&:gets).chomp.to_s
-        if @pass.empty?
-          puts "Could Not Be Empty"
-        elsif @pass==@npass
-          break
-        else
-          puts "Password doesn't Match"
-        end
-      end
-      acc1=Cashier.new(@fname,@lname,@idp,@usid,@pass)
-      acc1.add
-    when 5
-      puts "Cashiers List :"
-      cashier=[]
-      f1=File.open("Cashier.txt","a+")
-      f1.each do |line|
-        s=""
-        i=0
-        line.each_char do |c|
-          unless c=='$'
-            s+=c 
+        f1.close
+      when 3
+        puts "Deleting Accountant"
+        puts "Enter the User ID"
+        usid=gets.chomp.to_s
+        Accountant.delete(usid)
+      when 4
+        puts "Adding a Cashier"
+        
+        loop do
+          puts "Enter First Name"
+          @fname=gets.chomp.to_s
+          unless @fname.empty?
+            break
           else
-            cashier[i]=s
-            i+=1
-            s=""
+            puts "Could Not Be Empty"
           end
         end
-        h=Hash.new
-        h={"Cashier Id"=>"","Password"=>"","Id Proof No."=>"","Full Name"=>"","Last Name"=>""}
-        k=0
-        h.each_key { |key| 
-          h[key]=cashier[k] unless k==1
-        k+=1 }
-        h["Full Name"]=h["Full Name"].to_s+" "+h["Last Name"].to_s
-        h.delete "Last Name"
-        puts h
-      end
-      f1.close
-    when 6
-      puts "Deleting Cashier"
-      puts "Enter the User ID"
-      usid=gets.chomp.to_s
-      Cashier.delete(usid)
-    when 7
-      puts "Signing Up as Customer"
-    
-      loop do
-        puts "Enter First Name"
-        @fname=gets.chomp.to_s
-        unless @fname.empty?
-          break
-        else
-          puts "Could Not Be Empty"
-        end
-      end
 
-      loop do
-        puts "Enter Last Name"
-        @lname=gets.chomp.to_s
-        unless @lname.empty?
-          break
-        else
-          puts "Could Not Be Empty"
-        end
-      end
-
-      loop do
-        puts "Enter Id Proof No."
-        @idp=gets.chomp.to_s
-        unless @idp.empty?
-          break
-        else
-          puts "Could Not Be Empty"
-        end
-      end
-
-      loop do
-        puts "Enter User Id"
-        @usid=gets.chomp.to_s
-        unless @usid.empty?
-          flag=true
-
-          while flag do
-            c=0
-            f=File.open("Customer.txt","a+")
-            f.each do |line|
-              if line.match /^#{@usid}/
-                puts "User Id Already Taken"
-                c+=1
-              end
-            end
-            flag=false if c==0
-          end
-          f.close
-          break
-        else
-          puts "Could Not be Empty"
-        end
-      end
-
-      while true do
-        puts "Enter Your New Password"
-        @pass=STDIN.noecho(&:gets).chomp.to_s
-        puts "Confirm Password"
-        @npass=STDIN.noecho(&:gets).chomp.to_s
-        if @pass.empty?
-          puts "Could Not Be Empty"
-        elsif @pass==@npass
-          break
-        else
-          puts "Password doesn't Match"
-        end
-      end
-      @bal=1000
-      acc1=Customer.new(@fname,@lname,@idp,@usid,@pass,@bal)
-      acc1.add
-    when 8
-      puts "Customers List :"
-      cus=[]
-      f1=File.open("Customer.txt","a+")
-      f1.each do |line|
-        s=""
-        i=0
-        line.each_char do |c|
-          unless c=='$'
-            s+=c 
+        loop do
+          puts "Enter Last Name"
+          @lname=gets.chomp.to_s
+          unless @lname.empty?
+            break
           else
-            cus[i]=s
-            i+=1
-            s=""
+            puts "Could Not Be Empty"
           end
         end
-        h=Hash.new
-        h={"Customer Id"=>"","Password"=>"","Account No."=>"","Id Proof No."=>"","Full Name"=>"","Last Name"=>"","Balance"=>""}
-        k=0
-        h.each_key { |key| 
-        h[key]=cus[k] unless k==1
-        k+=1 }
-        h["Full Name"]=h["Full Name"].to_s+" "+h["Last Name"].to_s
-        h.delete "Last Name"
-        puts h
-      end
-      f1.close
-    when 9
-      puts "Deleting Customer"
-      puts "Enter the User ID"
-      usid=gets.chomp.to_s
-      Customer.delete(usid)
-    when 10
-      b=[]
-      cust_det=""
-      f=File.open("Admin.txt","a+")
-      f.each do |line|
-        if line.match /^#{$usid}/
+
+        loop do
+          puts "Enter Id Proof No."
+          @idp=gets.chomp.to_s
+          unless @idp.empty?
+            break
+          else
+            puts "Could Not Be Empty"
+          end
+        end
+
+        loop do
+          puts "Enter User Id"
+          @usid=gets.chomp.to_s
+          unless @usid.empty?
+            flag=true
+
+            while flag do
+              c=0
+              f=File.open("Cashier.txt","a+")
+              f.each do |line|
+                if line.match /^#{@usid}/
+                  puts "User Id Already Taken"
+                  c+=1
+                end
+              end
+              flag=false if c==0
+            end
+            f.close
+            break
+          else
+            puts "Could Not be Empty"
+          end
+        end
+
+        while true do
+          puts "Enter Your New Password"
+          @pass=STDIN.noecho(&:gets).chomp.to_s
+          puts "Confirm Password"
+          @npass=STDIN.noecho(&:gets).chomp.to_s
+          if @pass.empty?
+            puts "Could Not Be Empty"
+          elsif @pass==@npass
+            break
+          else
+            puts "Password doesn't Match"
+          end
+        end
+        acc1=Cashier.new(@fname,@lname,@idp,@usid,@pass)
+        acc1.add
+      when 5
+        puts "Cashiers List :"
+        cashier=[]
+        f1=File.open("Cashier.txt","a+")
+        f1.each do |line|
           s=""
-          l=0
-          cust_det=line
+          i=0
           line.each_char do |c|
             unless c=='$'
               s+=c 
             else
-              b[l]=s
-              l+=1
+              cashier[i]=s
+              i+=1
               s=""
             end
           end
-          break
-        end
-      end
-      f.close
-      
-      while true
-        puts "Enter Current Password"
-        pass=STDIN.noecho(&:gets).chomp.to_s
-        if a[1]==pass
+          h=Hash.new
+          h={"Cashier Id"=>"","Password"=>"","Id Proof No."=>"","Full Name"=>"","Last Name"=>""}
+          k=0
+          h.each_key { |key| 
+            h[key]=cashier[k] unless k==1
+            k+=1 }
+            h["Full Name"]=h["Full Name"].to_s+" "+h["Last Name"].to_s
+            h.delete "Last Name"
+            puts h
+          end
+          f1.close
+        when 6
+          puts "Deleting Cashier"
+          puts "Enter the User ID"
+          usid=gets.chomp.to_s
+          Cashier.delete(usid)
+        when 7
+          puts "Signing Up as Customer"
           
+          loop do
+            puts "Enter First Name"
+            @fname=gets.chomp.to_s
+            unless @fname.empty?
+              break
+            else
+              puts "Could Not Be Empty"
+            end
+          end
+
+          loop do
+            puts "Enter Last Name"
+            @lname=gets.chomp.to_s
+            unless @lname.empty?
+              break
+            else
+              puts "Could Not Be Empty"
+            end
+          end
+
+          loop do
+            puts "Enter Id Proof No."
+            @idp=gets.chomp.to_s
+            unless @idp.empty?
+              break
+            else
+              puts "Could Not Be Empty"
+            end
+          end
+
+          loop do
+            puts "Enter User Id"
+            @usid=gets.chomp.to_s
+            unless @usid.empty?
+              flag=true
+
+              while flag do
+                c=0
+                f=File.open("Customer.txt","a+")
+                f.each do |line|
+                  if line.match /^#{@usid}/
+                    puts "User Id Already Taken"
+                    c+=1
+                  end
+                end
+                flag=false if c==0
+              end
+              f.close
+              break
+            else
+              puts "Could Not be Empty"
+            end
+          end
+
           while true do
             puts "Enter Your New Password"
-            npass=STDIN.noecho(&:gets).chomp.to_s
+            @pass=STDIN.noecho(&:gets).chomp.to_s
             puts "Confirm Password"
-            npass2=STDIN.noecho(&:gets).chomp.to_s
-            if npass.empty?
+            @npass=STDIN.noecho(&:gets).chomp.to_s
+            if @pass.empty?
               puts "Could Not Be Empty"
-            elsif npass==npass2
+            elsif @pass==@npass
               break
             else
               puts "Password doesn't Match"
             end
           end
-          break
-        else
-          puts "Password not match"
-        end
+          @bal=1000
+          acc1=Customer.new(@fname,@lname,@idp,@usid,@pass,@bal)
+          acc1.add
+        when 8
+          puts "Customers List :"
+          cus=[]
+          f1=File.open("Customer.txt","a+")
+          f1.each do |line|
+            s=""
+            i=0
+            line.each_char do |c|
+              unless c=='$'
+                s+=c 
+              else
+                cus[i]=s
+                i+=1
+                s=""
+              end
+            end
+            h=Hash.new
+            h={"Customer Id"=>"","Password"=>"","Account No."=>"","Id Proof No."=>"","Full Name"=>"","Last Name"=>"","Balance"=>""}
+            k=0
+            h.each_key { |key| 
+              h[key]=cus[k] unless k==1
+              k+=1 }
+              h["Full Name"]=h["Full Name"].to_s+" "+h["Last Name"].to_s
+              h.delete "Last Name"
+              puts h
+            end
+            f1.close
+          when 9
+            puts "Deleting Customer"
+            puts "Enter the User ID"
+            usid=gets.chomp.to_s
+            Customer.delete(usid)
+          when 10
+            b=[]
+            cust_det=""
+            f=File.open("Admin.txt","a+")
+            f.each do |line|
+              if line.match /^#{$usid}/
+                s=""
+                l=0
+                cust_det=line
+                line.each_char do |c|
+                  unless c=='$'
+                    s+=c 
+                  else
+                    b[l]=s
+                    l+=1
+                    s=""
+                  end
+                end
+                break
+              end
+            end
+            f.close
+            
+            while true
+              puts "Enter Current Password"
+              pass=STDIN.noecho(&:gets).chomp.to_s
+              if a[1]==pass
+                
+                while true do
+                  puts "Enter Your New Password"
+                  npass=STDIN.noecho(&:gets).chomp.to_s
+                  puts "Confirm Password"
+                  npass2=STDIN.noecho(&:gets).chomp.to_s
+                  if npass.empty?
+                    puts "Could Not Be Empty"
+                  elsif npass==npass2
+                    break
+                  else
+                    puts "Password doesn't Match"
+                  end
+                end
+                break
+              else
+                puts "Password not match"
+              end
+            end
+            cd=cust_det
+            cust_det=cust_det.gsub(/#{pass}/,npass)
+            File.write("Admin.txt",File.open("Admin.txt",&:read).gsub(cd,cust_det))
+          when 0
+            break
+          else puts "Wrong Choice"
+          end
+        end   
       end
-      cd=cust_det
-      cust_det=cust_det.gsub(/#{pass}/,npass)
-      File.write("Admin.txt",File.open("Admin.txt",&:read).gsub(cd,cust_det))
-    when 0
-      break
-    else puts "Wrong Choice"
-    end
-  end   
-end
